106fd4e9d4892191bf32e8f8ead12fd538908939
Sean Perryman <se@nperryman.com>
2015-12-30 12:04:22 -0800

    Data validation

--- schedule_handler.php
+++ schedule_handler.php
@@ -6,40 +6,42 @@
 			isset($_POST['homeScore']) &&
 			isset($_POST['awayScore']))
 	{
-		if ($_POST['homeTeam'] == "none" || $_POST['awayTeam'] == "none") die("You much chose both home and away teams.");
-		$homeTeam = mysqli_real_escape_string($link, $_POST['homeTeam']);
-		$awayTeam = mysqli_real_escape_string($link, $_POST['awayTeam']);
-		$gameDate = mysqli_real_escape_string($link, $_POST['gameDate']);
-		$gameTime = mysqli_real_escape_string($link, $_POST['gameTime']);
-		$homeScore = mysqli_real_escape_string($link, $_POST['homeScore']);
-		$awayScore = mysqli_real_escape_string($link, $_POST['awayScore']);
+		if ($_POST['homeTeam'] == "none" || $_POST['awayTeam'] == "none") echo "<div class=\"alert alert-danger\" role=\"alert\">You must choose both a home and away team.</div>";
+		else {
+			$homeTeam = mysqli_real_escape_string($link, $_POST['homeTeam']);
+			$awayTeam = mysqli_real_escape_string($link, $_POST['awayTeam']);
+			$gameDate = mysqli_real_escape_string($link, $_POST['gameDate']);
+			$gameTime = mysqli_real_escape_string($link, $_POST['gameTime']);
+			$homeScore = mysqli_real_escape_string($link, $_POST['homeScore']);
+			$awayScore = mysqli_real_escape_string($link, $_POST['awayScore']);
 
-		//Query for home team ID
-		$home_query = "SELECT id FROM Teams WHERE name ='" . $homeTeam . "'";
-		if ($home_result = mysqli_query($link, $home_query)) {
-			while ($row = mysqli_fetch_assoc($home_result)) {
-				$homeTeam = $row['id'];
-			}
-		} else die("Unable to pull home team id");
+			//Query for home team ID
+			$home_query = "SELECT id FROM Teams WHERE name ='" . $homeTeam . "'";
+			if ($home_result = mysqli_query($link, $home_query)) {
+				while ($row = mysqli_fetch_assoc($home_result)) {
+					$homeTeam = $row['id'];
+				}
+			} else die("Unable to pull home team id");
 
-		//Query for away team ID
-		$away_query = "SELECT id FROM Teams WHERE name ='" . $awayTeam . "'";
-		if ($away_result = mysqli_query($link, $away_query)) {
-			while ($row = mysqli_fetch_assoc($away_result)) {
-				$awayTeam = $row['id'];
-			}
-		} else die("Unable to pull away team id");
+			//Query for away team ID
+			$away_query = "SELECT id FROM Teams WHERE name ='" . $awayTeam . "'";
+			if ($away_result = mysqli_query($link, $away_query)) {
+				while ($row = mysqli_fetch_assoc($away_result)) {
+					$awayTeam = $row['id'];
+				}
+			} else die("Unable to pull away team id");
 
-		//Convert game time to 24h format
-		$gameTime = date("H:i:s", strtotime($gameTime));
+			//Convert game time to 24h format
+			$gameTime = date("H:i:s", strtotime($gameTime));
 
-		$insert_query = "INSERT INTO Schedule 
-										(home, away, game_date, game_time, home_score, away_score) VALUES 
-										('" . $homeTeam . "', '" . $awayTeam . "', '" . $gameDate . "', '" . $gameTime . "', '" . $homeScore . "', '" . $awayScore . "')";
-		
-		//echo( "Insert Query: " . $insert_query);								
+			$insert_query = "INSERT INTO Schedule 
+											(home, away, game_date, game_time, home_score, away_score) VALUES 
+											('" . $homeTeam . "', '" . $awayTeam . "', '" . $gameDate . "', '" . $gameTime . "', '" . $homeScore . "', '" . $awayScore . "')";
+			
+			//echo( "Insert Query: " . $insert_query);								
 
-		if ($result = mysqli_query($link, $insert_query)) echo "<div class=\"alert alert-success\" role=\"alert\">Success.</div>";
-		else echo "<div class=\"alert alert-danger\" role=\"alert\">Failure.</div>";
+			if ($result = mysqli_query($link, $insert_query)) echo "<div class=\"alert alert-success\" role=\"alert\">Success.</div>";
+			else echo "<div class=\"alert alert-danger\" role=\"alert\">Failure.</div>";
+		}
 	}
 ?>
