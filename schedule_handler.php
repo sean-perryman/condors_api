6abb606035afd3451f96cdcdfdbb10ea2e72788d
Sean Perryman <se@nperryman.com>
2015-12-30 11:37:25 -0800

    Issues with SQL insert

--- schedule_handler.php
+++ schedule_handler.php
@@ -27,6 +27,8 @@
 			}
 		} else die("Unable to pull away team id");
 
+		echo("Home Team ID: " . $homeTeam . "<br />Away Team ID: " . $awayTeam);
+
 		$insert_query = "INSERT INTO Schedule 
 										(home, away, game_date, game_time, home_score, away_score) VALUES 
 										(" . $homeTeam . ", " . $awayTeam . ", " . $gameDate . ", " . $gameTime . ", " . $homeScore . ", " . $awayScore . ")";
