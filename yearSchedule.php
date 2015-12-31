02feb92b993cc3d1ff0598bd0ae15ca5feec8c24
Sean Perryman <se@nperryman.com>
2015-12-31 09:58:32 -0800

    Think I've got the SQL query sorted

--- yearSchedule.php
+++ yearSchedule.php
@@ -2,11 +2,11 @@
 	require_once('dblink.php');
 	$schedule = array();
 
-	$query = "SELECT Schedule.home, Schedule.away, Schedule.game_date FROM Schedule AS s
-						INNER JOIN Teams AS t ON s.home=t.id
-						AND s.away=t.id						
-						WHERE game_date BETWEEN '2015-09-01'
-						AND '2016-08-01' 
+	$query = "SELECT s.game_date, h.name, a.name 
+						FROM Schedule AS s 
+						INNER JOIN Teams h ON s.home=h.id 
+						INNER JOIN Teams a ON s.away=a.id 
+						WHERE s.game_date BETWEEN '2015-09-01' AND '2016-08-01' 
 						ORDER BY game_date ASC";
 
 	$result = mysqli_query( $link, $query );
