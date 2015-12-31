d0cf8ee26f208f232fdfd69b3d594c079b3d5b03
Sean Perryman <se@nperryman.com>
2015-12-30 16:37:20 -0800

    Working out the SQL query needed to pull the names instead of the team ID

--- yearSchedule.php
+++ yearSchedule.php
@@ -1,12 +1,23 @@
 <?php
 	require_once('dblink.php');
 	$schedule = array();
-	$result = mysqli_query( $link, "SELECT * FROM Schedule WHERE game_date BETWEEN '2015-09-01'	AND '2016-08-01' ORDER BY game_date ASC" );
+
+	$query = "SELECT Schedule.home, Schedule.away, Schedule.game_date FROM Schedule AS s
+						INNER JOIN Teams AS t ON s.home=t.id
+						AND s.away=t.id						
+						WHERE game_date BETWEEN '2015-09-01'
+						AND '2016-08-01' 
+						ORDER BY game_date ASC";
+
+	$result = mysqli_query( $link, $query );
 	$i = 0;
 	while ($row = mysqli_fetch_assoc($result)) {
 		array_push($schedule, $row);
 		$i++;
 	}
+
+
+
 	print_r(json_encode($schedule));
 	mysqli_close($link);
 ?>
